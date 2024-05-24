82a93794a0332348879e56f0b0e6c48d13fa50d9
Pablo <pablo@zoocha.com>
2024-05-23 22:21:28 -0300

    Implement dependency injection

--- web/modules/custom/movie_db/src/Controller/SearchController.php
+++ web/modules/custom/movie_db/src/Controller/SearchController.php
@@ -3,14 +3,33 @@
 namespace Drupal\movie_db\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\movie_db\Services\MovieDBIntegration;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  *
  */
 class SearchController extends ControllerBase {
 
+  /**
+   * @var \Drupal\movie_db\Services\MovieDBIntegration
+   */
+  protected MovieDBIntegration $apiClient;
+
+  public function __construct(MovieDBIntegration $apiClient) {
+    $this->apiClient = $apiClient;
+  }
+
+  /**
+   * @{@inheritdoc}
+   */
+  public static function create(ContainerInterface $container){
+    return new static(
+      $container->get('movie_db.api_integration')
+    );
+  }
   public function search($category = NULL, $query = NULL) {
-    $results = \Drupal::service('movie_db.api_integration')->searchMovie($query)['results'];
+    $results = $this->apiClient->searchMovie($query)['results'];
     return [
       '#theme' => 'movie_db_search_results_template',
       '#results' => $results,
@@ -21,7 +40,6 @@ class SearchController extends ControllerBase {
   }
 
   public function fetchMovieDetails($movie_id) {
-
 
   }
 }
--- web/modules/custom/movie_db/src/Plugin/Block/SearchMoviesBlock.php
+++ web/modules/custom/movie_db/src/Plugin/Block/SearchMoviesBlock.php
@@ -7,7 +7,10 @@ namespace Drupal\movie_db\Plugin\Block;
 
 use Drupal\Core\Block\Attribute\Block;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a block with Movie trends of the day
@@ -17,14 +20,35 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
   admin_label: new TranslatableMarkup("Search Movies"),
   category: new TranslatableMarkup("Search Form")
 )]
-class SearchMoviesBlock extends BlockBase {
+class SearchMoviesBlock extends BlockBase implements ContainerFactoryPluginInterface{
 
   /**
+   * The form builder.
    *
+   * @var \Drupal\Core\Form\FormBuilderInterface
    */
+  protected $formBuilder;
 
+  public function __construct(array $configuration,
+                              $plugin_id,
+                              $plugin_definition,
+                              FormBuilderInterface $formBuilder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $formBuilder;
+  }
+  public static function create(ContainerInterface $container,
+                                array $configuration,
+                                $plugin_id,
+                                $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder'),
+    );
+  }
 
   public function build() {
-    return \Drupal::formBuilder()->getForm('Drupal\movie_db\Form\MovieDBSearchForm');
+    return $this->formBuilder->getForm('Drupal\movie_db\Form\MovieDBSearchForm');
   }
 }
